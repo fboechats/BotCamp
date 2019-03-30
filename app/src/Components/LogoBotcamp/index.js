31b517dae0f867fed0abdd55ffd4d3fb6f1bbae9
root <root@DellsonDaSilva.localdomain>
2019-03-30 17:50:17 -0300

    Colocando a Logo feliz

---
+++ app/src/Components/LogoBotcamp/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.png";
+import "./style.css";
+
+const LogoBotcamp = () => (
+    <img src={ logo } className="logo-botcamp" alt="Logo do Botcamp" ></img>
+);
+
+export default LogoBotcamp;
